13d727924b25bf87f33c6698bfe4395e54fa2a99
Hamza Shahbaz <hamzashahbaz@live.com>
2020-02-23 20:17:46 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
-import TodoList from './components/TodoList';
+import React, { Component } from 'react'
+import TodoList from './components/TodoList'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div >
-        <TodoList/>
+      <div className='container'>
+        <div className='content'>
+          <TodoList/>
+        </div>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TodoForm from "./TodoForm";
-import Todo from "./Todo";
+import Todo from "./TodoItem";
 
 export default class TodoList extends React.Component {
   state = {
